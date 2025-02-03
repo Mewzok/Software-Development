db8fc5d3d84fbeca0240109f4432f90c12dfc510
mewzok <mewzok7@yahoo.com>
2025-02-03 13:59:04 -0700

    Add Chapter 9 folder to contain all chapter 9 .java files

---
+++ Module-1/Chapter 9/Rectangle.java
@@ -0,0 +1,24 @@
+public class Rectangle {
+    // declare variables, public for assignment
+    public double width;
+    public double height;
+
+    // default constructor
+    Rectangle() {
+        width = 1;
+        height = 1;
+    }
+
+    Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
