40259c7f8cda51bd449f1ed540e66f31785c422c
404kevnotfound <mewzok7@yahoo.com>
2025-02-18 11:06:15 -0700

    Add unfinished Exercise15_Listeners

---
+++ Module-5/Exercise15_Listeners/src/BallPane.java
@@ -0,0 +1,61 @@
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class BallPane extends Pane {
+    public final double radius = 20;
+    public final int moveIncrement = 50;
+    private double x = radius, y = radius;
+    private Circle circle = new Circle(x, y, radius);
+
+    public BallPane() {
+            circle.setStroke(Color.BLACK);
+            circle.setFill(Color.WHITE);
+            getChildren().add(circle);
+        }
+
+    protected void moveLeft() {
+        if((circle.getCenterX() - radius) > 0) {
+            circle.setCenterX(circle.getCenterX() - moveIncrement);
+            if(circle.getCenterX() - radius < 0) {
+                circle.setCenterX(0 + radius);
+            }
+        }
+    }
+
+    protected void moveRight() {
+        if((circle.getCenterX() + radius) < this.getWidth()) {
+            circle.setCenterX(circle.getCenterX() + moveIncrement);
+            if(circle.getCenterX() + radius > this.getWidth()) {
+                circle.setCenterX(this.getWidth() - radius);
+            }
+        }
+    }
+
+    protected void moveUp() {
+        if((circle.getCenterY() - radius) > 0) {
+            circle.setCenterY(circle.getCenterY() - moveIncrement);
+            if(circle.getCenterY() - radius < 0) {
+                circle.setCenterY(0 + radius);
+            }
+        }
+    }
+
+    protected void moveDown() {
+        if((circle.getCenterY() + radius) < this.getHeight()) {
+            circle.setCenterY(circle.getCenterY() + moveIncrement);
+            if(circle.getCenterY() + radius > this.getHeight()) {
+                circle.setCenterY(this.getHeight() - radius);
+            }
+        }
+    }
+
+    protected void resize() {
+        if(circle.getCenterX() - radius < 0) {
+            circle.setCenterX(0 + radius);
+        }
+        if(circle.getCenterX() + radius > this.getWidth()) {
+            circle.setCenterX(this.getWidth() - radius);
+        }
+    }
+}
---
+++ Module-5/Exercise15_Listeners/src/Exercise15_Listeners.java
@@ -0,0 +1,55 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class Exercise15_Listeners extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        // create panes and buttons
+        BorderPane borderPane = new BorderPane();
+        BallPane ballPane = new BallPane();
+        Button btLeft = new Button("Left");
+        Button btRight = new Button("Right");
+        Button btUp = new Button("Up");
+        Button btDown = new Button("Down");
+        HBox hBox = new HBox(5, btLeft, btRight, btUp, btDown);
+        hBox.setAlignment(Pos.CENTER);
+
+        borderPane.setCenter(ballPane);
+        borderPane.setBottom(hBox);
+
+        // create and register button handlers
+        btLeft.setOnAction(e -> {
+            ballPane.moveLeft();
+        });
+
+        btRight.setOnAction(e -> {
+            ballPane.moveRight();
+        });
+
+        btUp.setOnAction(e -> {
+            ballPane.moveUp();
+        });
+
+        btDown.setOnAction(e -> {
+            ballPane.moveDown();
+        });
+
+        // listener to prevent ball from going offscreen if window is resized
+        borderPane.widthProperty().addListener(ov -> ballPane.resize());
+
+        // create scene and place in stage
+        Scene scene = new Scene(borderPane, 200, 150);
+        primaryStage.setTitle("Exercise15_Listeners");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
