49762b5dd31e4cb2b45a2a6cf1501882e7eef743
Mewzok <mewzok7@yahoo.com>
2025-02-27 21:52:25 -0700

    Update main class logic to test saving and loading files, also add new classes to grid

--- Final-2/InvoiceApp/src/BarryGoldwaterInvoices.java
+++ Final-2/InvoiceApp/src/BarryGoldwaterInvoices.java
@@ -6,6 +6,7 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class BarryGoldwaterInvoices extends Application {
@@ -22,13 +23,31 @@ public class BarryGoldwaterInvoices extends Application {
         placeHeadersInRow(mainGrid);
 
         // example, delete
-        Invoice invoice = new Invoice("RK123", "OTB456", new BigDecimal("1500.00"),
-                new Date(), new Date(), new BigDecimal("200.00"),
-                new Date(), new Date(), new BigDecimal("50.00"),
-                new BigDecimal("100.00"), new BigDecimal("1150.00"));
+        // Invoice invoice = new Invoice("RK008-M", "N/A",
+        // new Broker("Broker Name", "123 Main St", "5555555555", 11.4,
+        // "fake@email.com", "brokerName",
+        // "poNumber"),
+        // new Shipper("Shipper Name", "123 Main St", "phonenumber", 81.5,
+        // "delivery address", "pickupDateandTime",
+        // 408.5, 4015791),
+        // new Receiver("Receiver Name", "123 Main St", "phonenumber", 81.5,
+        // "delivery address",
+        // "pickupDateandTime", 408.5, 4015791),
+        // new BigDecimal("1500.00"),
+        // new Date(), new Date(), new BigDecimal("200.00"),
+        // new Date(), new Date(), new BigDecimal("50.00"),
+        // new BigDecimal("100.00"), new BigDecimal("1150.00"));
 
-        addInvoiceToGrid(mainGrid, invoice);
+        ArrayList<Invoice> invoices = InvoiceStorage.loadInvoices();
+        // invoices.add(invoice);
+        // InvoiceStorage.saveInvoices(invoices);
 
+        invoices.get(0).getBroker();
+
+        // add all invoices to main grid
+        for (Invoice invoiceT : invoices) {
+            addInvoiceToGrid(mainGrid, invoiceT);
+        }
 
         // set and place main window scene
         Scene scene = new Scene(mainGrid);
@@ -37,25 +56,17 @@ public class BarryGoldwaterInvoices extends Application {
         primaryStage.show();
     }
 
-    private void loadFile() {
-
-    }
-
-    private void saveFile() {
-
-    }
-
     // place headers in first row, also determine size of each column
     private void placeHeadersInRow(GridPane mGrid) {
         // create header String array
         String[] headers = {
-                "RK Number", "OTB Number", "Gross", "Pickup Date", "Delivery Date",
+                "RK Number", "OTB Number", "Broker", "Shipper", "Receiver", "Gross", "Pickup Date", "Delivery Date",
                 "Factor Cost", "Factor Date", "Factor Due", "Dispatch Cost",
                 "OTB Cost", "Net"
         };
 
         // set header row
-        for(int col = 0; col < headers.length; col++) {
+        for (int col = 0; col < headers.length; col++) {
             StackPane cell = new StackPane(new Label(headers[col]));
             cell.setStyle("-fx-border-color: black; -fx-padding: 5; -fx-background-color: lightgray;");
             mGrid.add(cell, col, 0);
@@ -67,6 +78,9 @@ public class BarryGoldwaterInvoices extends Application {
         String[] invoiceData = {
                 inv.getRkNumber(),
                 inv.getOtbNumber(),
+                inv.getBroker().getCompanyName(),
+                inv.getShipper().getCompanyName(),
+                inv.getReceiver().getCompanyName(),
                 inv.getGross().toString(),
                 DateFormatter.formatDate(inv.getPickupDate()),
                 DateFormatter.formatDate(inv.getDeliveryDate()),
@@ -79,13 +93,13 @@ public class BarryGoldwaterInvoices extends Application {
 
         // set data row from invoice array
         double columnWidth = 100;
-        for(int col = 0; col < invoiceData.length; col++) {
+        for (int col = 0; col < invoiceData.length; col++) {
             StackPane cell = new StackPane(new Label(invoiceData[col]));
             cell.setStyle("-fx-border-color: black; -fx-padding: 5; -fx-background-color: white;");
             cell.setAlignment(Pos.CENTER);
             mGrid.add(cell, col, 1);
 
-            if(columnWidth < cell.getWidth()) {
+            if (columnWidth < cell.getWidth()) {
                 columnWidth = cell.getWidth();
             }
             ColumnConstraints columnConstraints = new ColumnConstraints();
